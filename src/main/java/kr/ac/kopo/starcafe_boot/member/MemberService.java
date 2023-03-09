10c487f7c897c72f053f86920fec4e30b1d355f0
hyeonmin <twingfull@gmail.com>
2023-03-09 13:36:46 +0900

    Member DELETE
    add html

--- src/main/java/kr/ac/kopo/starcafe_boot/member/MemberService.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.kopo.starcafe_boot.member;
-
-import kr.ac.kopo.starcafe_boot.entity.MemberEntity;
-
-public interface MemberService {
-    MemberEntity signin(MemberEntity member);
-
-    void login(MemberEntity member);
-}
